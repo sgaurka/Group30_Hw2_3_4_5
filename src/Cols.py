d81613686a9c8686953db4426b625b51b78d9469
ksjavali <112131763+ksjavali@users.noreply.github.com>
2022-09-14 00:23:46 -0400

    Create Cols.py

---
+++ src/Cols.py
@@ -0,0 +1,28 @@
+import re
+from Sym import Sym
+from numeric import Num
+from csv_parser import push
+
+class Cols:
+    def __init__(self,names=""):
+      self.names=names
+      self.all=[]
+      self.klass=None
+      self.x=[]
+      self.y=[]
+
+      for c, s in names.items():
+        if re.search("^[A-Z]",s)!=None:
+          col=push(self.all, Num(c,s))
+          col= push(self.all, Sym(c,s))
+        if re.search(":$",s)==None:
+          if re.search("[!+=]",s)!=None:
+            push(self.y,col)
+          else:
+            push(self.x,col)
+          if re.search("!$",s)!=None:
+            self.klass=col
+
+if __name__ == '__main__':
+    cols = Cols(Clndrs,Volume,Hp:,Lbs-,Acc+,Model,origin,Mpg+)
+  
